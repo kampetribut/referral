b8bdfef704a28ea769cd7b196093f4f1ff3a7232
kampetribut <kampretribut@gmail.com>
2018-07-10 01:05:26 +0700

    Update livetogel.js

--- livetogel.js
+++ livetogel.js
@@ -1,2 +1,2 @@
 
-document.write('<iframe frameborder="0" height="90%" src="http://datatogelwla.mobie.in/georgia-midday" width="100%"></iframe>')
+document.write('<iframe frameborder="0" height="100%" src="http://datatogelwla.mobie.in/georgia-midday" width="100%"></iframe>')
