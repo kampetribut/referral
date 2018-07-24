178b3e903d3c8029904948b03c850ba33a1fb77e
Kampret <kampretribut@gmail.com>
2018-07-24 18:41:21 +0700

    Create wla.js

---
+++ wla.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+document.write("<table style=\"width: 100%;\" border=\"1\" cellpadding=\"1\"><tbody>"); 
+document.writeln("<tr BGCOLOR='#F5DE35'> <td>NEGARA</td> <td>TOGEL</td> <td>BANDAR</td> <td>LIVE</td> <td>TUTUP</td> <td>PUTARAN</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>MAGNUM 4D</td> <td><a href=\"http://www.ace4d.org/link.php?member=magnum45\">BANDAR</a></td> <td><a href=\"http://www.magnum4d.my/en/\">18.45 Wib</a></td> <td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>SPORTS TOTO</td> <td><a href=\"http://www.ace4d.org/link.php?member=magnum45http://www.ace4d.org/link.php?member=magnum45\">BANDAR</a></td> <td><a href=\"http://www.sportstoto.com.my/\">18.45 Wib</a></td> <td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>DAMACAI</td> <td><a href=\"http://www.ace4d.org/link.php?member=magnum45\">BANDAR</a></td> <td><a href=\"https://www.damacai.com.my/home\">18.45 Wib</a></td> <td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>SANDAKAN 4D</td> <td><a href=\"http://www.javatoto.org/m/link.php?member=javajive\">BANDAR</a></td> <td><a href=\"http://www.stc4d.com/index.aspx\">19.00 Wib</a></td> <td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>SABAH LOTTO88 4D</td> <td><a href=\"http://www.javatoto.org/m/link.php?member=javajive\">BANDAR</a></td> <td><a href=\"http://www.lotto88.com.my/cms/\">19.00 Wib</a></td> <td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>MALAYSIA</td> <td>SARAWAK 4D CASHSWEEP</td> <td><a href=\"http://www.javatoto.org/m/link.php?member=javajive\">BANDAR</a></td> <td><a href=\"http://www.cashsweep.com.my/\">19.00 Wib</a></td><td>18.00 Wib</td> <td>RABU,SABTU,MINGGU</td></tr>"); 
+document.writeln("<tr> <td>FILIPINA</td> <td>PCSO</td> <td><a href=\"http://www.ace4d.org/link.php?member=magnum45\">BANDAR</a></td> <td><a href=\"http://www.pcso.gov.ph/SearchLottoResult.aspx\">20.10 Wib</a></td> <td>19:50 Wib</td> <td>MINGGU LIBUR</td></tr>"); 
+document.writeln("<tr> <td>SELANDIA BARU</td> <td>BULLS EYE</td> <td><a href=\"http://www.dewalive.biz/link.php?member=chery1010\">BANDAR</a></td> <td><a href=\"https://mylotto.co.nz/results/bullseye\">13.15 Wib</a></td> <td>12.45 Wib</td> <td>SETIAP HARI</td></tr>"); 
+document.writeln("<tr> <td>IRLANDIA</td> <td>DAILY MILLION 2 PM</td> <td>BANDAR</td> <td><a href=\"https://www.lottery.ie/dbg/results/view?game=daily-million\">21.00 Wib</a></td> <td>20.00 Wib</td> <td>SETIAP HARI</td></tr>"); 
+document.writeln("<tr> <td>IRLANDIA</td> <td>DAILY MILLION 9 PM</td> <td>BANDAR</td> <td><a href=\"https://www.lottery.ie/dbg/results/view?game=daily-million\">04.00 Wib</a></td> <td>03.00 Wib</td> <td>SETIAP HARI</td></tr>"); 
+document.writeln("<tr> <td>GERMANY</td> <td>GERMANY PLUS 5</td> <td>BANDAR</td> <td><a href=\"https://www.lotterypost.com/game/329/results\">00.10 Wib</a></td> <td>00.00 Wib</td> <td>SETIAP HARI</td></tr>"); 
+document.writeln("</tbody> </table>");
+</script>
