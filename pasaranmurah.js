e423c9775e2b049eff9c94267825f637813a4228
Kampret <kampretribut@gmail.com>
2026-03-20 22:27:49 +0700

    Create pasaranmurah.js

---
+++ pasaranmurah.js
@@ -0,0 +1,33 @@
+document.write('<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js' type='text/javascript'>
+</script><br />
+<script type='text/javascript'>
+$(document).ready(function() {
+$('img#closed').click(function(){
+$('#btm_banner').hide(90);
+});
+});
+</script>
+<style type='text/css'>
+div#btm_banner {
+bottom: 0;
+position: fixed;
+width: 100%;
+opacity: 0.9;
+left: 0;
+}
+div#btm_banner img{
+border:0;
+cursor:pointer;
+}
+</style>
+<div style='height: 0px;'></div>
+<div align='center' id='btm_banner' style='height: 120px; z-index: 9999;'>
+<div style='text-align: right; width: 930px; height: 10px;'>
+<img id='closed' src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgXx83YwsiVQxgT6XMtzuho7PCPwbBPXAF5wxTRwgbh-meqwzcA8afqeXc8eLoe_IDhqK5Y2qbvWtRGf3tyuAhUC8qWy8NqbZeDBKvGsud5lFqwgtSLxTGu575lGfTI-jMglrM_fjzbv8gS/s1600/close3.png'/></div>
+<div style='clear: both;'></div>
+<p>
+<p><center>
+
+<a href="https://www.pasaranmurah.com/_View/Register.aspx?ref=25BFE2" target="_blank"> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhYRuY2qyAezGaLsuOGS2uZ1myg1spIr_OdxzuwAXBSL11FahSfMMQdu-IW2pyA8FQoVtNPFfZOR5ppyS7z5oqPGjqL_Rrygythpf0yI5eJRJ45oUg5pSB8fjbroZHUH7jqYPX-_CbJapE/s1600/Jasa_Bola-0000.gif" alt="AGEN BOLA ONLINE" title="JASA BOLA" style="width:90%;height:100%;border:0" /></a></center></p></p></div>')
+
+
