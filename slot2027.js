cf72642d9f9373930ccd3a24423fed2c9154f5ae
Kampret <kampretribut@gmail.com>
2026-03-20 20:05:07 +0700

    Create slot2027.js

---
+++ slot2027.js
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html>
+<html b:css='false' b:defaultwidgetversion='2' b:layoutsVersion='3' expr:dir='data:blog.languageDirection' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<head>
+<meta charset='utf-8'/>
+<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+<meta content='blogger' name='generator'/>
+    <b:include data='blog' name='all-head-content'/>
+    <title><data:blog.pageTitle/></title>
+<meta content='ARSIKTOBA3' name='Author'/>
+<meta content='general' name='rating'/>
+<meta content='Indonesia' name='geo.country'/>
+<meta content='en_US' property='og:locale'/>
+<meta content='en_GB' property='og:locale:alternate'/>
+<meta content='Indonesia' name='language'/>
+<meta content='https://www.facebook.com/rudi.skid' property='article:author'/>
+<meta content='https://www.facebook.com/rui.skid' property='article:publisher'/>
+<meta content='APP-ID-Facebook' property='fb:app_id'/>
+<meta content='100002572025086' property='fb:admins'/>
+<meta content='@rudiskid' name='twitter:site'/>
+<meta content='@rudiskid' name='twitter:creator'/>
+    <b:skin><![CDATA[
+        /* Tulis CSS Anda di sini */
+        body {background-color: #fff; margin: 0; padding: 0;}
+  ]]></b:skin>
+<style type='text/css'>
+body{background:#051524;}
+#main-wrapper{background:#051524;border:none;}
+.footerbawah, h2.post-title{display:none;}
+.post-body{background:#051524;}
+h1{font-family: Arial; font-size: 26px; text-align:center; letter-spacing:3px; font-weight:bold;text-transform:uppercase;position:relative;color:#00FFFF}
+</style>
+<style>
+img{
+height:auto;
+max-width:100%
+}
+  </style>
+ </head>
+
+<body>
+  <b:section class='main' id='main' showaddelement='no'/>
+<center>
+   <div id='fb-root'/>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = &quot;//connect.facebook.net/en_US/all.js#xfbml=1&quot;;
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));</script>
+    <div class='fb-share-button' data-href='https://saranahoki3.blogspot.com/' data-type='button_count'/></center>
+<BR>
+<center><a href="https://slotdt.us/register?referral_code=rudi75" style="text-decoration: none" target="_blank"> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjdgnc0RK8x1m4-ql7H3n4-5PBDokEOl5lJ6K7zkYL5w3IrOCmkRI8Ihptlo-CaGXloVSJN5GApjKAelE1_A0OxX7m1-oryCmN2twunymmBIPhgs3qLb7Niy2dw5omUx4QVP0y-gb-Y_bIsj5LZzCL7Xxp_zecCuK86Y4OaDKH4dwtRJfl-rPAw9wSi8CXh/s600/1000311870.gif" alt="BANDAR SLOT" title=" BANDAR SLOT" style="width:100%;height:100%;border:0" /></a>
+
+<a href="https://menangslotmaxwin.blogspot.com" style="text-decoration: none" target="_blank"> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiGlmYJg0EwGwt3xgKo8AwCwJj1D1aNGtE3UN841wsH1n1OcOwx4sLWzvWk8oQNnuGzsXdFdn8NcCNu85DxNHw3wu0d9kmrausknVUt5DRhg8zdkjYHHEMSbH0mt5GjlYDZ5juGtGPhgQxDkub75O2VIx1qk8v8rOCvqZs9mqNfsD1IcRKEnN-KGIhH-f1W/s770/1000311871.png" alt="BANDAR SLOT" title=" BANDAR SLOT" style="width:100%;height:100%;border:0" /></a>
+
+<a href="https://menangslotmaxwin.blogspot.com" style="text-decoration: none" target="_blank"> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgiZ9nI_BauMprvdiyXFkGtfCVCEoNbf68HeMHJlc-UL6d1Cn35hshZBgMKr3cHTwTI0_f1c53EXOxvMVu_ndMMNZzz-wDqtk7fSuITnbPA0x9gkNYKfI5PYxn6q36XgOgThlXmW3lQjgITWw5mygWV7iV5DUDGrd5G6FOAyN0vL6e0Wjgre-UJjila09rm/s680/1000311872.png" alt="BANDAR SLOT" title=" BANDAR SLOT" style="width:100%;height:100%;border:0" /></a></BR>
+<BR>
+<center><font color='YELLOW'><b><script language="JavaScript"> Year=new
+Date(); var copyright=Year.getUTCFullYear(); document.write
+  ("Copyright © 2025 - " + copyright); </script></b></font></center>
+</BR>
+  </body>
+</html>
