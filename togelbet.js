271d0fd553cf2aab9c56c85edeb6ab639c300bd5
Kampret <kampretribut@gmail.com>
2026-03-21 00:20:04 +0700

    Create togelbet.js

---
+++ togelbet.js
@@ -0,0 +1,37 @@
+document.write('<a href="https://bandartotobet2.blogspot.com" target="_blank"> <img
+        src="https://lh3.googleusercontent.com/-DgUwtQxZDEk/VRlMfkQPcTI/AAAAAAAACR0/7tqXT8F3f6c/s2048/togel1%252520%25252815%252529.jpg"
+        width="140px" height="550px"
+        align="middle"
+        style="float: left;
+          position: relative;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          z-index: 1;"
+        ></a>
+
+<a href="https://bandartotobet2.blogspot.com" target="_blank"><img
+        src="https://lh4.googleusercontent.com/-vJW_XM1lr70/VRlMb_hTMoI/AAAAAAAACR0/GU2krdl4DE8/s2048/togel1%252520%25252813%252529.gif"
+        width="140px" height="550px"
+        align="middle"
+        style="float: right;
+          position: relative;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          z-index: 1;"
+        ></a>
+
+
+
+<a href="https://bandartotobet2.blogspot.com" target="_blank"><img
+        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3AbNK7EivILJQ3ETh9hbGeOHIL-Q7SOUVdFrOJVORiOgiKu8ZiIitPjiHNlDqjHjCaaQnV6m6b8J4Lw0b2zmJJKxTsJFQWo2J4yoPfN0us8xK_78QWxvceeW1tSLm4bg2vSIeJJEjoFDe/s2048/sexy-header.jpg"
+        width="100%" height="100%"
+        align="middle"
+        style="float: bottom;
+          position: relative;
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          z-index: 1;"
+        ></a>')
