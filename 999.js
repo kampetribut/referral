137873b859e4ff33652560d05495d815df3c03fd
Kampret <kampretribut@gmail.com>
2026-03-25 19:57:12 +0700

    Update 999.js

--- 999.js
+++ 999.js
@@ -1,4 +1,4 @@
-<script type='text/javascript'>
+document.write('<script type='text/javascript'>
         $(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#btm_banner&#39;).hide(90);});});
 </script>
     <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
@@ -6,4 +6,4 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kampetribut/referral@master/888.js"></script>
 <!--Akhir script iklan-->
         </div>
- </div><!--end: floating ads-->
+ </div><!--end: floating ads-->')
