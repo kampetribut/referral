dbdaecb114d64b2a63cdd660cd40b4dd2747ba32
Kampret <kampretribut@gmail.com>
2026-03-26 03:36:31 +0700

    Create 444.js

---
+++ 444.js
@@ -0,0 +1,10 @@
+document.write('<div id="floating_banner_top" style="background:transfarant;bottom: 0; height: auto; left: 0; padding: 0px; position: fixed; text-align: center; width: 100%; z-index: 1001;">
+                            <div id="adv_footer" align="center">
+                              <a href="https://bandartotobet2.blogspot.com/" style="cursor:pointer;" onclick="document.getElementById(&#39;adv_footer&#39;).style.display = &#39;none&#39;;">                              <img class="tutup" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinbfesavcZ_EXSG22eLHp-n7Y7yH-MJQNF6KAd2y0Ql30GiZHECIlMsOoC5GCDZwFiTuvdPeFHEjIdbHaJPk0I1xd1y4MQ75CbFmIOSaa6qDihpUNR6EmBEOrId7NffFJHqb5ZqKfQEZiH/s1600/PhoXo2.png" alt="Tutup" title="Tutup iklan ini" height="12" width="50" />
+</a>
+                              <br />
+                              <a href="https://bandartotobet2.blogspot.com/" target="_blank">                              <img alt="AGEN DOMINOQQ ONLINE" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg4z2oXxyP1WxXS0w_TYw51qUU9rMyAlwuPOKGCJ8NQos3D6QWXVvlmTAH4rxOoO8noX45Yp7yBvyk0JR86jTKGzDKILf54Dio4mWSq2Po6k5mEBL3J1S41UNoxjaO8P1QIFFa9jguf7OpZ/s1600/xlgirls.gif" height="90px" title="DOMINOBET" width="100%" />
+</a>
+                
+                            </div>
+                          </div>')
