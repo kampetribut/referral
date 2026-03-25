c2dced3e9c1f5b4f16dade729ebbe15e82e3ee7a
Kampret <kampretribut@gmail.com>
2026-03-25 21:20:42 +0700

    Delete 999.js

--- 999.js
+++
@@ -1,9 +0,0 @@
-document.write('<script type='text/javascript'>
-        $(document).ready(function() {$(&#39;img#closed&#39;).click(function(){$(&#39;#btm_banner&#39;).hide(90);});});
-</script>
-    <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
-        <!--Script iklan-->
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kampetribut/referral@master/888.js"></script>
-<!--Akhir script iklan-->
-        </div>
- </div><!--end: floating ads-->')
