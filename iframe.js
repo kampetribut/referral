cc6fc1d52d7bea972fa5fff4db6a2e1b8126723f
Kampret <kampretribut@gmail.com>
2026-03-29 15:16:45 +0700

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,2 +1,1 @@
 <iframe src="https://wgets.angkapaito.net/result.php?show_id=76,94,89" width="300" height="170" scrolling="no" style="border:0px;"></iframe>
-<iframe src="https://wgets.angkapaito.net/result.php?show_id=166,167,168,169,170" width="300" height="180" scrolling="no" style="border:0px;"></iframe>
