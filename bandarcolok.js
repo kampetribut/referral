e7ac0ba73be012fa946298395426f607b40f7f42
Kampret <kampretribut@gmail.com>
2026-03-31 23:14:03 +0700

    Update bandarcolok.js

--- bandarcolok.js
+++ bandarcolok.js
@@ -1,12 +1,1 @@
-<center>
-   <div id='fb-root'></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = &quot;//connect.facebook.net/en_US/all.js#xfbml=1&quot;;
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, &#39;script&#39;, &#39;facebook-jssdk&#39;));</script>
-    <div class='fb-share-button' data-href='https://saranahoki2.blogspot.com/' data-type='button_count'/></center>
-<br/>
-document.write('<a href="https://www.theccruise.com/register?referral=vina567" style="text-decoration: none" target="_blank"> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2aLPp84OqQ_rom4JJsVWixy7ezZfubBr0NyqrpeC441vtWIhf5WacsEqdoprF6T9vzTwe-cS3t8pEjaS4al05wsnpLR3yQ_oYx1Mq6qsrB-w1-UNkYLfwzFy2xoxd48x8Cchn3Wp-03weGXIr2jsAPIzTGqrzjF-7DGuoWXa5U5YT9U9qaMMI_ctWOII/s1600/1000323932.gif" alt="BANDAR SLOT" title=" BANDAR SLOT" style="width:100%;height:100%;border:0" /></a>')
+
